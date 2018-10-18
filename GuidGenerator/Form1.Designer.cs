9e2f4a7e7ef20973b4e94390c6c1ca3804a14092
junrikson <junrikson@gmail.com>
2018-10-18 20:23:21 +0700

    Initial commit

---
+++ GuidGenerator/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace GuidGenerator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnGenerate = new System.Windows.Forms.Button();
+            this.CmbTotal = new System.Windows.Forms.ComboBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // BtnGenerate
+            // 
+            this.BtnGenerate.Location = new System.Drawing.Point(236, 12);
+            this.BtnGenerate.Name = "BtnGenerate";
+            this.BtnGenerate.Size = new System.Drawing.Size(115, 23);
+            this.BtnGenerate.TabIndex = 0;
+            this.BtnGenerate.Text = "Generate";
+            this.BtnGenerate.UseVisualStyleBackColor = true;
+            this.BtnGenerate.Click += new System.EventHandler(this.BtnGenerate_Click);
+            // 
+            // CmbTotal
+            // 
+            this.CmbTotal.FormattingEnabled = true;
+            this.CmbTotal.Items.AddRange(new object[] {
+            "10",
+            "100",
+            "1000"});
+            this.CmbTotal.Location = new System.Drawing.Point(12, 12);
+            this.CmbTotal.Name = "CmbTotal";
+            this.CmbTotal.Size = new System.Drawing.Size(218, 21);
+            this.CmbTotal.TabIndex = 1;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 41);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(339, 270);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(363, 323);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.CmbTotal);
+            this.Controls.Add(this.BtnGenerate);
+            this.Name = "Form1";
+            this.Text = "Guid Generator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnGenerate;
+        private System.Windows.Forms.ComboBox CmbTotal;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ GuidGenerator/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GuidGenerator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnGenerate_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "";
+            int.TryParse(CmbTotal.Text, out int total);
+
+            for(int i = 0; i < total; i++)
+            {
+                richTextBox1.AppendText(Guid.NewGuid().ToString() + "\n");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CmbTotal.Text = "10";
+        }
+    }
+}
